51ca5e6ffb21bb49e362a6c1c52f622565073ebf
mohamad dabboussi <dmohamad@hotmail.com>
2020-05-10 17:41:22 +0300

    1

---
+++ Erc1/Classes/Mission.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+using Erc1.Model;
+using System.Collections;
+using System.Data;
+
+namespace Erc1.Classes
+{
+    class Mission
+    {
+        public static IEnumerable Getالمراكز() 
+        {
+            
+
+
+                using (ERCEntities entity = new ERCEntities())
+                {
+                var c = (
+                from marakez in entity.المراكز
+                join manate2 in entity.المناطق
+                on marakez.المنطقة equals manate2.رمز
+                select new
+                {
+                   manate2= manate2.المنطقة
+                }
+                    );
+                    return c.ToList();
+                };
+            
+        }
+
+        public static void insertMission(int الرمز_الشهري, DateTime التاريخ,int الآلية, int المريض, Nullable<int> من_مشفى, Nullable<int> من_القسم, Nullable<int> الطبيب_المعالج,
+           Nullable<int> الجهة_الضامنة, Nullable<int> إلى_مشفى, Nullable<int> إلى_القسم, int مسؤول_المهمة, int مسعف_1,int مسعف_2,
+            int السائق, Nullable<int> المتصل, string الهاتف, Nullable<int> متلقي_المهمة, int رمز_الحالة, string رمز_السنوي,
+             int السنة, string تفاصيل_ال_من, string تفاصيل_ال_الى, Nullable<int> من_رمز_المدينة, Nullable<int> من_رمز_المنطقة,
+             Nullable<int> الى_رمز_المدينة, Nullable<int> الى_رمز_المنطقة, string التفاصيل, string اسم_المتصل,
+              int طبيعة_المهمة, string رمز__المركز)
+        {
+            using (ERCEntities entity =new Model.ERCEntities())
+            {
+                المهمات_المنفذة newmission = new المهمات_المنفذة()
+                {
+                    الرمز_الشهري = الرمز_الشهري,
+                    التاريخ=  التاريخ,
+                    الآلية= الآلية,
+                    المريض= المريض ,
+                    من_مشفى= من_مشفى,
+                    من_القسم= من_القسم,
+                    الطبيب_المعالج= الطبيب_المعالج,
+                    الجهة_الضامنة= الجهة_الضامنة,
+                    إلى_مشفى= إلى_مشفى,
+                    إلى_القسم= إلى_القسم,
+                    مسؤول_المهمة= مسؤول_المهمة,
+                    مسعف_1= مسعف_1,
+                    مسعف_2= مسعف_2,
+                    السائق= السائق,
+                    المتصل= المتصل,
+                    الهاتف= الهاتف,
+                    متلقي_المهمة= متلقي_المهمة,
+                    رمز_الحالة= رمز_الحالة,
+                    رمز_السنوي= رمز_السنوي,
+                    السنة= السنة,
+                    تفاصيل_ال_من= تفاصيل_ال_من,
+                    تفاصيل_ال_الى= تفاصيل_ال_الى,
+                    من_رمز_المدينة= من_رمز_المدينة,
+                    من_رمز_المنطقة= من_رمز_المنطقة,
+                    الى_رمز_المدينة= الى_رمز_المدينة,
+                    الى_رمز_المنطقة= الى_رمز_المنطقة,
+                    التفاصيل= التفاصيل,
+                    اسم_المتصل= اسم_المتصل,
+                    طبيعة_المهمة= طبيعة_المهمة,
+                    رمز__المركز= رمز__المركز
+                };
+                entity.المهمات_المنفذة.Add(newmission);
+                entity.SaveChanges();
+            };
+        }
+    }
+}
