4a39fbcce7ed715a4e0a1eda9534f4a2d76772d3
mohamad dabboussi <dmohamad@hotmail.com>
2020-07-09 09:55:05 +0300

    DAL 9-7-2020

--- Erc1/DAL/DAL.cs
+++ Erc1/DAL/DAL.cs
@@ -778,5 +778,205 @@ namespace Erc1.Classes
         }
 
 
+
+
+        //    9 - 7 - 2020     //
+        //Add member team to a mission
+        public static bool add_Mission_Memeber(الفريق mission_member)
+        {
+            bool added = false;
+            using (ERCEntities entity = new ERCEntities())
+            {
+                try
+                {
+                    entity.الفريق.Add(mission_member);
+                    added = entity.SaveChanges() > 0 ? true : false; ;
+                    return added;
+                }
+                catch
+                {
+                    added = false;
+                    return added;
+                }
+            };
+
+        }
+
+
+        // check if patient exist
+        public static bool Check_Patient(int patientID)
+        {
+            int rep;
+            bool e;
+            using (ERCEntities entity = new model.ERCEntities())
+            {
+                try
+                {
+                    rep = entity.المرضى
+                      .Where(r => r.الرمز == patientID)
+                      .Count()
+                      ;
+                    if (rep == 0) e = false;
+                    else e = true;
+                    return e;
+                }
+                catch
+                {
+                    e = false;
+                    return e;
+                }
+            };
+
+        }
+
+
+
+
+
+
+
+
+        // return int إلى if no error occured and -1 if there is an error
+        public static int Get_from_Region(int regionID)
+        {
+            int rep;
+            using (ERCEntities entity = new ERCEntities())
+            {
+                try
+                {
+                    if (entity.من.Count((r => r.من1 == regionID && r.مستشفى_منطقة == false)) == 0)
+                    {
+                        من fom = new من();
+                        fom.مستشفى_منطقة = false;
+                        fom.من1 = regionID;
+                        entity.من.Add(fom);
+                        entity.SaveChanges();
+                        rep = fom.الرمز;
+                    }
+                    else
+                    {
+                        rep = entity.من.Where((r => r.من1 == regionID && r.مستشفى_منطقة == false)).First().الرمز;
+                    }
+                    return rep;
+                }
+                catch
+                {
+                    rep = -1;
+                    return rep;
+                }
+            };
+
+        }
+
+
+        // return int إلى if no error occured and -1 if there is an error
+        public static int Get_to_Region(int regionID)
+        {
+            int rep;
+            using (ERCEntities entity = new ERCEntities())
+            {
+                try
+                {
+                    if (entity.إلى.Count((r => r.إلى1 == regionID && r.مستشفى_منطقة == false)) == 0)
+                    {
+                        إلى fom = new إلى();
+                        fom.مستشفى_منطقة = false;
+                        fom.إلى1 = regionID;
+                        entity.إلى.Add(fom);
+                        entity.SaveChanges();
+                        rep = fom.الرمز;
+                    }
+                    else
+                    {
+                        rep = entity.إلى.Where((r => r.إلى1 == regionID && r.مستشفى_منطقة == false)).First().الرمز;
+                    }
+                    return rep;
+                }
+                catch
+                {
+                    rep = -1;
+                    return rep;
+                }
+            };
+
+        }
+
+
+
+        // return int من if no error occured and -1 if there is an error
+        public static int Get_from_Hospital_Section(int hosID, int secID)
+        {
+            int rep;
+            int hos_secID;
+            using (ERCEntities entity = new ERCEntities())
+            {
+                try
+                {
+                    hos_secID = entity.المستشفيات_مع_اقسام.Where((r => r.رمز_المشفى == hosID && r.رمز_القسم == secID)).Select(r => r.الرمز).FirstOrDefault();
+
+                    if (entity.من.Count((r => r.من1 == hos_secID && r.مستشفى_منطقة == true)) == 0)
+                    {
+                        من fom = new من();
+                        fom.مستشفى_منطقة = true;
+                        fom.من1 = hos_secID;
+                        entity.من.Add(fom);
+                        entity.SaveChanges();
+                        rep = fom.الرمز;
+                    }
+                    else
+                    {
+                        rep = entity.من.Where((r => r.من1 == hos_secID && r.مستشفى_منطقة == true)).First().الرمز;
+                    }
+                    return rep;
+                }
+                catch
+                {
+                    rep = -1;
+                    return rep;
+                }
+            };
+
+        }
+
+
+
+        // return int إلى if no error occured and -1 if there is an error
+        public static int Get_to_Hospital_Section(int hosID, int secID)
+        {
+            int rep;
+            int hos_secID;
+            using (ERCEntities entity = new ERCEntities())
+            {
+
+                hos_secID = entity.المستشفيات_مع_اقسام.Where((r => r.رمز_المشفى == hosID && r.رمز_القسم == secID)).Select(r => r.الرمز).FirstOrDefault();
+                try
+                {
+                    if (entity.إلى.Count((r => r.إلى1 == hos_secID && r.مستشفى_منطقة == true)) == 0)
+                    {
+                        إلى fom = new إلى();
+                        fom.مستشفى_منطقة = true;
+                        fom.إلى1 = hos_secID;
+                        entity.إلى.Add(fom);
+                        entity.SaveChanges();
+                        rep = fom.الرمز;
+                    }
+                    else
+                    {
+                        rep = entity.إلى.Where((r => r.إلى1 == hos_secID && r.مستشفى_منطقة == true)).First().الرمز;
+                    }
+                    return rep;
+                }
+                catch
+                {
+                    rep = -1;
+                    return rep;
+                }
+            };
+
+        }
+
+
+
+
     }
 }
