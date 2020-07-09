daad4fe1719425d6a7b3d1bb1c4dfa4a96d1b870
mohamad dabboussi <dmohamad@hotmail.com>
2020-07-09 23:01:17 +0300

    9-7-2020

--- Erc1/DAL/DAL.cs
+++ Erc1/DAL/DAL.cs
@@ -904,6 +904,46 @@ namespace Erc1.Classes
         }
 
 
+        public static bool delete_Mission_Team_Cases(int الرمز_الشهري, int رمز_السنوي, int السنة, int رمز_المركز)
+        {
+            bool delete1, delete2, delete3, delete;
+            delete1 = delete2 = delete3 = false;
+            using (var context = new ERCEntities())
+            {
+                try
+                {
+                    المهمات_المنفذة mission = context.المهمات_المنفذة.Where(x => ((x.الرمز_الشهري == الرمز_الشهري) && (x.رمز_السنوي == رمز_السنوي) && (x.السنة == السنة) && (x.رمز__المركز == رمز_المركز))).Single<المهمات_المنفذة>();
+                    context.المهمات_المنفذة.Remove(mission);
+                    context.SaveChanges();
+                    delete1 = true;
+                }
+                catch { delete1 = false; }
+
+
+                try
+                {
+                    context.حالات_المهمات.RemoveRange(context.حالات_المهمات.Where(x => ((x.الرمز_الشهري == الرمز_الشهري) && (x.رمز_السنوي == رمز_السنوي) && (x.السنة == السنة))));
+                    context.SaveChanges();
+                    delete2 = true;
+                }
+                catch { delete2 = false; }
+
+
+
+
+                try
+                {
+                    context.الفريق.RemoveRange(context.الفريق.Where(x => ((x.الرمز_الشهري == الرمز_الشهري) && (x.رمز_السنوي == رمز_السنوي) && (x.السنة == السنة) && (x.رمز__المركز == رمز_المركز))));
+                    context.SaveChanges();
+                    delete3 = true;
+                }
+                catch { delete3 = false; }
+
+                delete = delete1 && delete2 && delete3;
+                return delete;
+            }
+        }
+
     }
 
     class Hospital
